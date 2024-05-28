5b971dc84a48d40412c3b629b408839af1b2309d
ArslanArshad105 <60978049+ArslanArshad105@users.noreply.github.com>
2024-05-28 21:07:22 +0500

    created ui cards 06

--- src/app/cards-05/page.js
+++ src/app/cards-05/page.js
@@ -1,66 +1,25 @@
 import React from "react";
 import "../cards-05/style.css";
-// import { dataCards04 } from "@/utlis/cardsData";
+import { dataCards05 } from "@/utlis/cardsData";
 
 const Cards05 = () => {
   return (
     <div class="cards">
-      <figure class="card">
-        <div class="corner left-top"></div>
-        <div class="corner left-bottom"></div>
-        <div class="corner right-top"></div>
-        <div class="corner right-bottom"></div>
-        <img
-          src="/assets/media/cards05/lakeview-elegance.png"
-          alt="Lakeview Elegance"
-        />
-        <figcaption>
-          <h3>Lakeview Elegance</h3>
-          <p>258 Serenity Lane, Crestwood Hills</p>
-        </figcaption>
-      </figure>
-      <figure class="card">
-        <div class="corner left-top"></div>
-        <div class="corner left-bottom"></div>
-        <div class="corner right-top"></div>
-        <div class="corner right-bottom"></div>
-        <img
-          src="/assets/media/cards05/vista-paradiso.png"
-          alt="Vista Paradiso"
-        />
-        <figcaption>
-          <h3>Vista Paradiso</h3>
-          <p>123 Paradise Road, Lakeview</p>
-        </figcaption>
-      </figure>
-      <figure class="card">
-        <div class="corner left-top"></div>
-        <div class="corner left-bottom"></div>
-        <div class="corner right-top"></div>
-        <div class="corner right-bottom"></div>
-        <img
-          src="/assets/media/cards05/balcony-appartment.jpg"
-          alt="Balcony Apartment"
-        />
-        <figcaption>
-          <h3>Garden Vista</h3>
-          <p>123 Tranquil Terrace Greenfield Grove</p>
-        </figcaption>
-      </figure>
-      <figure class="card">
-        <div class="corner left-top"></div>
-        <div class="corner left-bottom"></div>
-        <div class="corner right-top"></div>
-        <div class="corner right-bottom"></div>
-        <img
-          src="/assets/media/cards05/skyline-oasis.png"
-          alt="Skyline Oasis"
-        />
-        <figcaption>
-          <h3>Skyline Oasis</h3>
-          <p>450 Metropolitan Ave, Downtown</p>
-        </figcaption>
-      </figure>
+      {dataCards05.map((item) => {
+        return (
+          <figure class="card">
+            <div class="corner left-top"></div>
+            <div class="corner left-bottom"></div>
+            <div class="corner right-top"></div>
+            <div class="corner right-bottom"></div>
+            <img src={item.card_image} alt={item.alt_text} />
+            <figcaption>
+              <h3>{item.title}</h3>
+              <p>{item.description}</p>
+            </figcaption>
+          </figure>
+        );
+      })}
     </div>
   );
 };
