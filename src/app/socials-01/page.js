4e2684884842e38314f2c7151c4ad80c63c3ee87
ArslanArshad105 <60978049+ArslanArshad105@users.noreply.github.com>
2024-06-21 21:41:09 +0500

    Created Socials 01

---
+++ src/app/socials-01/page.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "../Socials-01/style.css";
+
+const Socials = () => {
+  return (
+    <span className="background">
+      <span className="social-media-buttons">
+        <span className="social-media-button">
+          <img alt="Instagram" src="assets/media/socials-01/instagram.svg" />
+        </span>
+        <span className="social-media-button">
+          <img alt="Twitter" src="assets/media/socials-01/twitter.svg" />
+        </span>
+        <span className="social-media-button">
+          <img alt="Youtube" src="assets/media/socials-01/youtube.svg" />
+        </span>
+        <span className="social-media-button">
+          <img alt="LinkedIn" src="assets/media/socials-01/linkedin.svg" />
+        </span>
+      </span>
+    </span>
+  );
+};
+
+export default Socials;
